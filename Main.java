fb5cd7a6cf82f05e41c3ae9b9de57d2e0fa68dfd
Mohomed Ajmal <ajmalmohomed544@gmail.com>
2025-08-14 09:56:58 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //circle a1 = new circle(5.0 , "blue");
-        cylinder c1 = new cylinder(20.0 );
-
-        System.out.println(c1.toString());
-    }
-}
